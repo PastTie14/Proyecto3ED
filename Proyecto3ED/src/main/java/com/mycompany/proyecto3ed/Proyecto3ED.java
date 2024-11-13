2f7ba8565e5c6c05227cb6f92a7a12b4eff7d562
Ian López Z <104876938+PastTie14@users.noreply.github.com>
2024-11-12 23:15:44 -0600

    ProyectoSubido

---
+++ Proyecto3ED/src/main/java/com/mycompany/proyecto3ed/Proyecto3ED.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto3ed;
+
+/**
+ *
+ * @author Ian
+ */
+public class Proyecto3ED {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

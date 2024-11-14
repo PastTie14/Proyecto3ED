ba6021adb9cca8484c3467db3e62fb0cf0fc9daf
Salvador Morán Aguilar <166891691+salvmoran@users.noreply.github.com>
2024-11-14 13:31:36 -0600

    Subo cositas

---
+++ Proyecto3ED/src/main/java/Islas/Componente.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Islas;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author PALA
+ */
+public abstract class Componente {
+    private String nombre;
+    private int costo;
+    private int vida;
+    private boolean estado;
+    private int cantFila;
+    private int cantColumna;
+    ImageIcon imagen;
+    
+    // constructor
+    
+}
---
+++ Proyecto3ED/src/main/java/Islas/Conector.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Islas;
+
+import java.util.List;
+
+/**
+ *
+ * @author PALA
+ */
+public class Conector {
+    List<Componente> componentesConectados;
+}
